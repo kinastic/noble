7baa709e6ef50b22efc7150978a728d2fe8667cf
locorider <rota.jose@gmail.com>
2018-02-14 10:56:08 +0100

    removed comments write support

--- examples/write-example.js
+++ examples/write-example.js
@@ -4,11 +4,12 @@ const NobleFactory = require('../index');
 const noble = NobleFactory(0, false);
 noble.init();
 
-const addresses = ['cd:89:6c:f6:86:47', 'e7:fe:e0:76:fa:c1', 'e8:3b:b5:33:3c:4c', 'e8:b9:1a:26:5b:3a']; // 000026
+// const addresses = ['cd:89:6c:f6:86:47', 'e7:fe:e0:76:fa:c1', 'e8:3b:b5:33:3c:4c', 'e8:b9:1a:26:5b:3a']; // 000026
 // const peripheralIdOrAddress = 'fa:df:6d:8d:87:fe'; // 000103
-const address = 'e7:fe:e0:76:fa:c1'; // 000110
+// const address = 'e7:fe:e0:76:fa:c1'; // 000110
 // e8:3b:b5:33:3c:4c // 000112
 // e8:b9:1a:26:5b:3a // 000104
+const address = 'cd:89:6c:f6:86:47'; // 000026
 const found = [];
 
 noble.on('stateChange', (state) => {
@@ -20,61 +21,49 @@ noble.on('stateChange', (state) => {
 });
 
 noble.on('discover', async (peripheral) => {
-  if (found.length !== addresses.length && addresses.findIndex(a => a === peripheral.address) !== -1
-    && found.findIndex(p => p.address === peripheral.address) === -1) {
-    found.push(peripheral);
-  } else if(found.length === addresses.length) {
-    await noble.stopScanning();
+  if (address === peripheral.address) {
+    noble.stopScanning();
 
-    for (const peripheral of found) {
-      console.log('peripheral with ID ' + peripheral.address + ' found');
-      const advertisement = peripheral.advertisement;
+    console.log('peripheral with ID ' + peripheral.address + ' found');
+    const advertisement = peripheral.advertisement;
 
-      const localName = advertisement.localName;
-      const txPowerLevel = advertisement.txPowerLevel;
-      const manufacturerData = advertisement.manufacturerData;
-      const serviceData = advertisement.serviceData;
-      const serviceUuids = advertisement.serviceUuids;
+    const localName = advertisement.localName;
+    const txPowerLevel = advertisement.txPowerLevel;
+    const manufacturerData = advertisement.manufacturerData;
+    const serviceData = advertisement.serviceData;
+    const serviceUuids = advertisement.serviceUuids;
 
-      if (localName) {
-        console.log('  Local Name        = ' + localName);
-      }
-
-      if (txPowerLevel) {
-        console.log('  TX Power Level    = ' + txPowerLevel);
-      }
-
-      if (manufacturerData) {
-        console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
-      }
-
-      if (serviceData) {
-        console.log('  Service Data      = ' + serviceData);
-      }
-
-      if (serviceUuids) {
-        console.log('  Service UUIDs     = ' + serviceUuids);
-      }
-
-      console.log();
-
-      startExploring(peripheral);
+    if (localName) {
+      console.log('  Local Name        = ' + localName);
     }
+
+    if (txPowerLevel) {
+      console.log('  TX Power Level    = ' + txPowerLevel);
+    }
+
+    if (manufacturerData) {
+      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+    }
+
+    if (serviceData) {
+      console.log('  Service Data      = ' + serviceData);
+    }
+
+    if (serviceUuids) {
+      console.log('  Service UUIDs     = ' + serviceUuids);
+    }
+
+    console.log();
+
+    startExploring(peripheral);
   }
 });
 
 const startExploring = (peripheral) => {
-  peripheral.source.subscribe(({ event, payload }) => {
-    if (event === 'disconnect') {
-      setTimeout(() => {
-        explore(peripheral);
-      }, 500);
-    }
-  });
   explore(peripheral);
 };
 
-const explore = async (peripheral) =>
+const explore = async (peripheral) => {
   console.log('services and characteristics:');
 
   try {
@@ -97,7 +86,7 @@ const explore = async (peripheral) =>
       for (const characteristic of characteristics) {
         let characteristicInfo = '  ' + characteristic.uuid;
 
-        if (characteristic.uuid === 'f0000003de94078fe31135b1ee4fdb15') {
+        if (characteristic.uuid === 'f0000007de94078fe31135b1ee4fdb15') {
           chara = characteristic;
         }
 
@@ -123,10 +112,11 @@ const explore = async (peripheral) =>
       }
     }
     if (chara) {
-      const subscription = chara.source.subscribe(({ payload }) => {
-        console.log(payload);
-      });
-      await chara.subscribe();
+      const data = await chara.read();
+      const { data: arr } = data.toJSON();
+      console.log(`DATA: `, arr);
+      arr[0] = 4;
+      // await chara.write(new Buffer(arr));
 
       // setTimeout(async () => {
       //   subscription.unsubscribe();
@@ -135,6 +125,7 @@ const explore = async (peripheral) =>
     }
   } catch (err) {
     console.error(err);
+  } finally {
     peripheral.disconnect();
   }
 };
